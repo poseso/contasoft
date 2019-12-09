b5cc5be89c64bda177a080486683019182dcb400
poseso <carlosmsanchez@gmail.com>
2019-12-08 20:21:27 -0400

    - move KTAppOptions outside layouts

--- resources/assets/js/webpack/scripts.js
+++ resources/assets/js/webpack/scripts.js
@@ -7,6 +7,24 @@
  */
 
 // Core Plugins
+window.KTAppOptions = {
+    "colors": {
+        "state": {
+            "brand": "#2c77f4",
+            "light": "#ffffff",
+            "dark": "#282a3c",
+            "primary": "#5867dd",
+            "success": "#34bfa3",
+            "info": "#36a3f7",
+            "warning": "#ffb822",
+            "danger": "#fd3995"
+        },
+        "base": {
+            "label": ["#c5cbe3", "#a1a8c3", "#3d4465", "#3e4466"],
+            "shape": ["#f0f3ff", "#d9dffa", "#afb4d4", "#646c9a"]
+        }
+    }
+};
 window.KTUtil = require("../global/components/base/util");
 window.KTApp = require("../global/components/base/app");
 window.KTAvatar = require("../global/components/base/avatar");
