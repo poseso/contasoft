f8c3b0c85c9c7535ddc9208b9d3d77c8b965959b
Carlos Sanchez <carlosmsanchez@gmail.com>
2019-12-10 15:41:17 -0400

    - added models

---
+++ app/Models/Auth/UserLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Auth;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserLog extends Model implements \Altek\Accountant\Contracts\Ledger
+{
+    use \Altek\Accountant\Ledger;
+
+    protected $table = 'ledgers';
+
+    protected $casts = [
+        'properties' => 'json',
+        'modified' => 'json',
+        'pivot' => 'json',
+        'extra' => 'json',
+    ];
+
+    public function recordableUser()
+    {
+        return $this->belongsTo(User::class, 'recordable_id');
+    }
+}
