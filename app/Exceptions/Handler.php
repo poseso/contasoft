65ee7ddbbf7dbef97d9185c9b4a31d9af5d3c0d8
poseso <carlosmsanchez@gmail.com>
2019-12-15 22:36:29 -0400

    - catch errors in handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,21 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+/**
+ * Class Handler.
+ */
 class Handler extends ExceptionHandler
 {
     /**
@@ -13,7 +26,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        GeneralException::class,
     ];
 
     /**
@@ -29,7 +42,10 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param Exception $exception
+     *
+     * @throws Exception
+     * @return mixed|void
      */
     public function report(Exception $exception)
     {
@@ -39,12 +55,54 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param Request  $request
+     * @param Exception $exception
+     * @return Response
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof UnauthorizedException) {
+            return response(view('errors.401'));
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response(view('errors.403'));
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()
+                ->route('frontend.auth.login')
+                ->withFlashDanger(__('Su sesión ha expirado. Favor iniciar sesión nuevamente.'));
+        }
+
+        if ($exception instanceof HttpException) {
+            $statusCode = $exception->getStatusCode();
+
+            if (view()->exists('errors.'.$statusCode)) {
+                return response(view('errors.'.$statusCode, [
+                    'msg' => $exception->getMessage(),
+                    'code' => $statusCode,
+                ]), $statusCode);
+            }
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response(view('errors.404'));
+        }
+
         return parent::render($request, $exception);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException  $exception
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+            ? response()->json(['message' => 'Unauthenticated.'], 401)
+            : redirect()->guest(route('frontend.auth.login'));
     }
 }
